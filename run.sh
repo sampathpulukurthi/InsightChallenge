d4ba432eabbb7f5c12125950f72bcc8a352843bd
sampathpulukurthi <pulukurthisampath@gmail.com>
2016-04-04 23:19:12 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # example of the run script for running the word count
 
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
-python ./src/Twitter_final_updated.py
+python ./src/Average_degree.py
 
 
 
